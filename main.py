4b6354d34abf53a51c3a21d4620a2e88b07482b7
Thierno <mamyourydiallo@gmail.com>
2019-02-08 10:53:11 +0100

    updated requirements file

--- main.py
+++ main.py
@@ -14,25 +14,25 @@ class WebsocketRequest(Resource):
             return "websocket not created or found"
 
 
-class NotificationFacture(Resource):
-    def get(self, userId):
+class NotificationBill(Resource):
+    def get(self, userid):
         # retrieve from dfs
         urls = []
 
-        response = {"userId": userId, "bills": [
+        response = {"userId": userid, "bills": [
             {"billId": "test", "date": "test", "uri": "http://maven.apache.org/archives/maven-1.x/maven.pdf"},
             {"billId": "", "date": "", "uri": "https://www.cfasup2000.fr/media/20190123115038_jpo_2019-bd.pdf"}]}
         return jsonify(response)
 
 
-class NotificationFactureTest(unittest.TestCase):
+class NotificationBillTest(unittest.TestCase):
     def test_bills(self):
-        res = NotificationFacture.get("1")
+        res = NotificationBill.get("1")
         self.assertIn({}, {})
 
 
 app = Flask(__name__)
 api = Api(app)
-api.add_resource(NotificationFacture, "/bills/<int:userId>")
+api.add_resource(NotificationBill, "/bills/<int:userid>")
 api.add_resource(WebsocketRequest, "/ws/<int:id>")
 app.run(host='0.0.0.0', port='5000')
