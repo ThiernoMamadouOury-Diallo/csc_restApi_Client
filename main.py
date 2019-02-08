e11d08165f1527fc984682c78915aae027927034
Thierno <mamyourydiallo@gmail.com>
2019-02-08 02:55:52 +0100

    first rest template for bills urls

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import json
+import unittest
+
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource, reqparse
 import requests
@@ -10,7 +13,22 @@ class WebsocketRequest(Resource):
         else:
             return "websocket not created or found"
 
+class NotificationFacture(Resource):
+    def get(self, id):
+        # retrieve from dfs
+        urls = []
+
+        response = {"id": id, "bills": [{"id": "test", "date": "test", "uri": "http://maven.apache.org/archives/maven-1.x/maven.pdf"}, {"id": "", "date": "", "uri": ""}]}
+        return json.dumps(response)
+
+class NotificationFactureTest(unittest.TestCase):
+    def test_bills(self):
+        res = NotificationFacture.get("1")
+        self.assertIn({}, {})
+
+
 app = Flask(__name__)
 api = Api(app)
+api.add_resource(NotificationFacture, "/bills/<int:id>")
 api.add_resource(WebsocketRequest, "/ws/<int:id>")
 app.run(host='0.0.0.0', port='5000')
